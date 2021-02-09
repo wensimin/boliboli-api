d8921ab047d47a6986c637bee56d4bed1a2b7c7e
wensimin <hukaizhiyu2@gmail.com>
2021-02-10 07:21:09 +0800

    Initial commit

---
+++ src/main/kotlin/tech/shali/boliboliapi/BoliboliApiApplication.kt
@@ -0,0 +1,11 @@
+package tech.shali.boliboliapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BoliboliApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<BoliboliApiApplication>(*args)
+}
