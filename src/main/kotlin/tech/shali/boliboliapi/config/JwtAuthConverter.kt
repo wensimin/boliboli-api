b78fa015bdde2846a2807247ac5a502764f8f0b2
wensimin <hukaizhiyu2@gmail.com>
2021-03-05 07:01:54 +0800

    code fix

--- src/main/kotlin/tech/shali/boliboliapi/config/JwtAuthConverter.kt
+++ src/main/kotlin/tech/shali/boliboliapi/config/JwtAuthConverter.kt
@@ -13,9 +13,6 @@ class JwtAuthConverter : Converter<Jwt, Collection<GrantedAuthority>> {
         // scope & auth 本项目 混用
         val scope = source.getClaim<Collection<String>?>("scope")
         val auth = source.getClaim<Collection<String>?>("auth")
-        val list = ArrayList<String>()
-        list.addAll(scope ?: emptyList())
-        list.addAll(auth ?: emptyList())
-        return list.map { s -> SimpleGrantedAuthority(s) }
+        return ((scope ?: emptyList()) + (auth ?: emptyList())).map { s -> SimpleGrantedAuthority(s) }
     }
 }
