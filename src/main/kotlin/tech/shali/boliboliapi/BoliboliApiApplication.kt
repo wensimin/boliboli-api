a648a6afe63119a3036043f094faa97d080ee553
wensimin <hukaizhiyu2@gmail.com>
2021-03-16 15:09:56 +0800

    删除无用注解

--- src/main/kotlin/tech/shali/boliboliapi/BoliboliApiApplication.kt
+++ src/main/kotlin/tech/shali/boliboliapi/BoliboliApiApplication.kt
@@ -3,12 +3,10 @@ package tech.shali.boliboliapi
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.scheduling.annotation.EnableScheduling
 
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableScheduling
 class BoliboliApiApplication
 
 fun main(args: Array<String>) {
